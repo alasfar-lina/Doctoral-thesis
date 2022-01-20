e6b5c4f23982ba4c763cc2ecdf2302162984b55b
alasfar-lina <lina.alasfar@outlook.fr>
2022-01-20 18:28:14 +0100

    20.1

--- HiggsCouplingMeasurmentsComb.R
+++ HiggsCouplingMeasurmentsComb.R
@@ -9,7 +9,7 @@ library(tidyverse)
 data = data.frame(
   'Exp'=c('ATLAS','CMS','ATLAS','CMS','ATLAS','CMS','ATLAS','CMS','ATLAS','CMS','ATLAS','CMS','ATLAS','CMS','ATLAS','CMS'),
   'id'= c('ka','ka','kg','kg','kz','kz','kw','kw','kt','kt','kb','kb','kta','kta','kmu','kmu'),
-  'k'=c( 1.04,0.895, 0.92,1.165,0.99,0.96,1.06,-1.225,0.92,1.01,0.87,0.95,0.92,0.94,1.07,0.21 ),
+  'k'=c( 1.04,0.895, 0.92,1.165,0.99,0.96,1.06,1.225,0.92,1.01,0.87,0.95,0.92,0.94,1.07,0.21 ),
   'std'= c(0.06,0.115, 0.065,0.115, 0.06,0.07,0.06,0.115,0.10,0.11,0.11,0.23, 0.07,0.12,0.275,0.71) )
 data2 <- data %>% group_by(Exp) %>% nest()
 
@@ -17,22 +17,22 @@ data2 <- data %>% group_by(Exp) %>% nest()
 
 aggrivated<- agg(id=id, es=k, var=std, method = "BHHR", cor = 0.5,  mod=NULL, data=data)
 
-#pdf(file = "./higgs_couplings.pdf", width = 7.5, height = 5.5)
+pdf(file = "./higgs_couplings.pdf", width = 7.5, height = 5.5)
 ggplot(aggrivated) + 
-  geom_boxplot(aes(x=reorder(id,-var),lower=es-var,upper=es+var,middle=es,ymin=es-2*var,ymax=es+2*var,fill = (var)*100),stat="identity")+
+  geom_boxplot(aes(x=reorder(id,var),lower=es-var,upper=es+var,middle=es,ymin=es-2*var,ymax=es+2*var,fill = (var)*100),stat="identity")+
   labs(x = 'Coupling', y = 'Value') + 
-  scale_x_discrete(labels= unname(TeX(c("$\\kappa_{\\mu}$","$\\kappa_{b}$","$\\kappa_{t}$","$\\kappa_{\\tau}$","$\\kappa_{g}$","$\\kappa_{\\gamma}$","$\\kappa_{W}$","$\\kappa_{Z}$")))) +
+  scale_x_discrete(labels= unname(TeX(rev(c("$\\kappa_{\\mu}$","$\\kappa_{b}$","$\\kappa_{t}$","$\\kappa_{\\tau}$","$\\kappa_{g}$","$\\kappa_{\\gamma}$","$|\\kappa_{W}|$","$\\kappa_{Z}$")))) )+
   coord_flip() + 
   theme_minimal() + 
   scale_fill_continuous(low = '#dfe9ec', high = '#3f7f93', name = "Uncertainty [%]", trans = "log10") +
  theme(axis.text = element_text(size = 16))  +
  theme(axis.title = element_text(size = 18)) +
-theme(legend.title = element_text(size = 16)) #+
-  #annotate("text", x=4.5, y=0.01, label= "ATLAS+CMS",size=6)+
-#annotate("text", x=4., y=0.015, label='Aggregated LHC Run-II',size=4)+
-#annotate("text", x=3.65, y=0.01, label=TeX('$L=139+137\\,\\mathrm{fb}^{-1}$'),size=4)
+theme(legend.title = element_text(size = 16)) +
+  annotate("text", x=4.5, y=0.11, label= "ATLAS+CMS",size=6)+
+annotate("text", x=4., y=0.120, label='Aggregated LHC Run-II',size=4)+
+annotate("text", x=3.65, y=0.11, label=TeX('$L=139+137\\,\\mathrm{fb}^{-1}$'),size=4)
 #annotate("text", x=3.15, y=0.01, label=TeX('$\\rho=0$'),size=4)
-#dev.off()
+dev.off()
 
 percent_first <- function(x) {
   x <- sprintf("%.1f%%", x)
